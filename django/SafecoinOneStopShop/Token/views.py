e98fa64462eaddb5efe46790f0c43a81b789eaba
jonnyboi <jonathan.wright@networkrail.co.uk>
2023-01-04 20:19:24 +0000

    update web

--- django/SafecoinOneStopShop/Token/views.py
+++ django/SafecoinOneStopShop/Token/views.py
@@ -4,9 +4,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    print(request.POST)
-    return render(request, "training/training.html", {'Answer': my_answer})
-    
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #print(request.POST)
+    return render(request, "Home.html")
+
 def Token(request):
     return HttpResponse("Token")
+
+def images(request):
+    return render(request, "images/SafeCoin_Icon.png")
